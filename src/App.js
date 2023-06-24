7a04f28e73960cfb2c04c53535ecc13600c2c8a9
jmahajan117 <jay.mahajan33@gmail.com>
2023-06-24 16:54:16 -0400

    Added Top Bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import {Navbar, Nav} from "react-bootstrap";
+import {Container} from "react-bootstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+  };
+
+  
+  render() {
+    return (
+      <>
+        <Navbar bg="light">
+          <Container>
+            <Nav>
+                <Navbar.Brand href="/">Jay Mahajan</Navbar.Brand>
+                <Navbar.Brand>Publications</Navbar.Brand>
+                <Navbar.Brand href="https://github.com/jmahajan117">GitHub</Navbar.Brand>
+              </Nav>
+            </Container>
+        </Navbar>
+      </>
+    )
+  }
+
+
 }
 
 export default App;
